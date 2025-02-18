392698c1e55bf632ed1d039564ae9f8d2f6e4d1b
atishnikam <68553361+atishnikam@users.noreply.github.com>
2025-02-18 10:05:57 +0530

    Create student.cs

---
+++ student.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authentication;
+
+namespace WebApplication1
+{
+    public class student
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+   
+}
